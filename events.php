d8a9bf0f2645a5572f04629f49d81e0c73ae27db
Joel Smith <jsumnersmith@gmail.com>
2013-01-26 15:18:51 -0500

    Removing the_content() from the events page.

--- events.php
+++ events.php
@@ -41,7 +41,7 @@ Template Name: Events
             <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
             <?php if ( has_post_thumbnail() ) { ?>
               <div class="event-excerpt">
-                <?php the_excerpt(); ?>
+                <?php //the_excerpt(); ?>
               </div>
               <?php } else { } ?>
               </section>
